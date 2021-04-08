d223e3edfbe8e202a97521a864fc3b70fd81c7d7
Johnatan Alexis Urbano Guzmán <johnurbaguz@gmail.com>
2021-04-08 09:54:07 -0500

    Changes to remove feature and rename current branch to master branch with base code

--- app/routes.js
+++ app/routes.js
@@ -1,9 +1,9 @@
-const weetsController = require('./controllers/weets');
+// const controller = require('./controllers');
 const { healthCheck } = require('./controllers/healthCheck');
 
 exports.init = app => {
   app.get('/health', healthCheck);
-  app.post('/weet', [], weetsController.createWeet);
+  // app.get('/endpoint/put/path', [], controller.methodGET);
   // app.put('/endpoint/put/path', [], controller.methodPUT);
   // app.post('/endpoint/post/path', [], controller.methodPOST);
 };
